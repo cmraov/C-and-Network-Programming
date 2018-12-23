7e2626e246eaf2844ee71cfc4e6380e75317d7f9
cmraov <45944915+cmraov@users.noreply.github.com>
2018-12-23 23:15:45 +0530

    Add files via upload

---
+++ combinetwostring.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+main()
+{
+  char str1[] = "abcd";
+  char str2[] = "efghijk";
+  char str3[100];
+  memset(str3,0,100);
+  int i = 0,j=0;
+  while(str1[i]!='\0' || str2[i] !='\0')
+   {
+     if(str1[i]!='\0' && str2[i] !='\0')
+       {
+        str3[j] =str1[i];
+        str3[++j] = str2[i];
+       }
+      if(str1[i]!='\0' && str2[i] =='\0')
+        str3[j] =str1[i];
+      else
+        str3[j] =str2[i];
+    i++;
+    j++;
+   }
+  printf("Combined string is %s\n",str3);
+}
